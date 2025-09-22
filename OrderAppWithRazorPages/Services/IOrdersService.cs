d3683054f020ed71bacc3a9bf24019553a31a0e7
Christian Joseph Dalisay <dev.christianjoseph@gmail.com>
2025-09-22 10:08:26 +0800

    Initial commit

---
+++ OrderAppWithRazorPages/Services/IOrdersService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using OrderApp.Models;
+
+namespace OrderApp.Services
+{
+    public interface IOrdersService
+    {
+        Order GetOrder();
+        List<(Product product, int quantity)> GetOrderDetails(int OrderId);
+        decimal GetOrderTotal(int OrderId);
+        (string Message, int Step) GetShippingStatus(int OrderId);
+    }
+}
